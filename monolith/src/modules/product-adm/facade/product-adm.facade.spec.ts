df10d2fcbef22d45f5f166f5aa4d3fa278215921
Pedro Assis <pedroo16_@hotmail.com>
2024-05-16 19:00:24 -0300

    add store-catalog on monolith module

--- monolith/src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ monolith/src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,9 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../repository/product.model";
-import ProductRepository from "../repository/product.repository";
-import AddProductUseCase from "../usecase/add-product/add-product.usecase";
-import ProductAdmFacade from "./product-adm.facade";
-import Product from "../domain/product.entity";
 import ProductAdmFacadeFactory from "../factory/facade.factory";
 
 describe('ProductAdmFacade test', () => {
