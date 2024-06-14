1f89f2a982ee992b0cda57ac73eaeb57819516ff
Pedro Assis <pedroo16_@hotmail.com>
2024-06-14 17:55:24 -0300

    (wip) kafka with go

--- kafka/go/cmd/producer/main.go
+++ kafka/go/cmd/producer/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
 	defer producer.Close()
-	Publish("Hello, Kafka!", "test", producer, nil)
+	Publish("Hello, Kafka!", "test", producer, nil, deliveryChan)
+	go DeliveryReport(deliveryChan) // async
+
 	producer.Flush(1000)
 }
 
@@ -26,17 +30,32 @@ func NewKafkaProducer() *kafka.Producer {
 	return producer
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	kafkaMessage := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
 
-	err := producer.Produce(kafkaMessage, nil)
+	err := producer.Produce(kafkaMessage, deliveryChan)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Error sending message")
+			} else {
+				fmt.Println("Message sent to topic", ev.TopicPartition)
+				// save on database that message was processed
+				// ex: confirm that a bank transaction was made
+			}
+		}
+	}
 }
