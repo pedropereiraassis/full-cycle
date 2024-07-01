95dbbe1ea387ada52ae89ba1e365bfedaa88675b
Pedro Assis <pedroo16_@hotmail.com>
2024-07-01 15:03:41 -0300

    (wip) keycloak module

---
+++ keycloak/authentication-flow/authorization-code-flow/src/index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import crypto from 'crypto';
+
+const app = express();
+
+app.get('/login', (req, res) => {
+  const loginParams = new URLSearchParams({
+    client_id: 'fullcycle-client',
+    redirect_uri: 'http://localhost:3000/callback',
+    response_type: 'code',
+    scope: 'openid',
+  });
+
+  const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`;
+  console.log(url);
+  res.redirect(url);
+});
+
+app.get('/callback', async (req, res) => {
+  console.log(req.query);
+
+  const bodyParams = new URLSearchParams({
+    client_id: 'fullcycle-client',
+    grant_type: 'authorization_code',
+    code: req.query.code as string,
+    redirect_uri: 'http://localhost:3000/callback',
+  });
+
+  const url = `http://host.docker.internal:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: bodyParams.toString(),
+  });
+
+  const result = await response.json();
+
+  console.log(result);
+
+  res.json(result);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
