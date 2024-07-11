3d1ec3ddde9e85e8ad05f3f997c30d0c1a2d6db8
Pedro Assis <pedroo16_@hotmail.com>
2024-07-11 14:44:05 -0300

    (wip) finish category repository - codeflix backend

--- codeflix/backend/src/category/infra/db/category-in-memory.repository.ts
+++ codeflix/backend/src/category/infra/db/category-in-memory.repository.ts
@@ -1,12 +1,38 @@
+import { SortDirection } from '../../../shared/domain/repository/search-params'
 import { UUID } from '../../../shared/domain/value-objects/uuid.value-object'
-import { InMemoryRepository } from '../../../shared/infra/db/in-memory/in-memory.repository'
+import { InMemorySearchableRepository } from '../../../shared/infra/db/in-memory/in-memory.repository'
 import { Category } from '../../domain/category.entity'
 
-export class CategoryInMemoryRepository extends InMemoryRepository<
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<
   Category,
   UUID
 > {
+  sortableFields: string[] = ['name', 'created_at']
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string | null
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items
+    }
+
+    return items.filter((item) => {
+      return item.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+
   getEntity(): new (...args: any[]) => Category {
     return Category
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Category[] {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, 'created_at', 'desc')
   }
 }
